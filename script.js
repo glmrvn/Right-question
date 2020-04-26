ce7b9d535a3143cd7a04331cb07c2fb23fe4c7cc
glmrvn <glmrvn@gmail.com>
2020-04-26 20:58:35 +0300

    Added new questions

--- script.js
+++ script.js
@@ -13,10 +13,31 @@ var questions = [
     'What do you like to do on weekends?',
     'What would you like to eat?',
     'What’s the weather like?',
-    'What are your views on Donald Trump as the America’s President?'
+    'What are your views on Donald Trump as the America’s President?',
+    'Do you play any sports around here?',
+    'How do you like working for your company?',
+    'When and where were you born?',
+    'What is your marital status?',
+    'What do you like?',
+    'What are you going to do tomorrow?',
+    'What will you do now?',
+    'What are you thinking of?',
+    'Are you happy today?',
+    'Can You Play Musical Instruments?',
+    'How Long Have You Been Studying English?',
+    'Do You Have Any Brothers And Sisters?',
+    'Have You Been To San Francisco?'
 ];
+
+var lastUsed = "";
 
 function newTitle () {
     var i = (Math.random() * questions.length) | 0;
-    question.innerText = questions[i];
+    if (i != lastUsed) {
+        var random = questions[i];
+        question.innerText = random;
+        lastUsed = i;
+    } else {
+        newTitle ()
+    }
 }
